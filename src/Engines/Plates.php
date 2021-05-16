4e2ef7e8e3512979ef7f94981d7fa11c2a18dc7a
Puleeno Nguyen <puleeno@gmail.com>
2021-05-16 23:01:17 +0700

    Fix coding standard

--- src/Engines/Plates.php
+++ src/Engines/Plates.php
@@ -3,30 +3,37 @@ namespace Jankx\TemplateEngine\Engines;
 
 use Jankx\TemplateEngine\Engine;
 
-class Plates extends Engine {
+class Plates extends Engine
+{
     const ENGINE_NAME = 'plates';
 
-    public function __construct() {
+    public function __construct()
+    {
     }
 
-    public function getName() {
+    public function getName()
+    {
         return static::ENGINE_NAME;
     }
 
-    public function setDefaultTemplateDir($dir) {
+    public function setDefaultTemplateDir($dir)
+    {
     }
 
-    public function setDirectoryInTheme($dirName) {
+    public function setDirectoryInTheme($dirName)
+    {
     }
 
-    public function searchTemplate($templates) {
-
+    public function searchTemplate($templates)
+    {
     }
 
-    public function render($templates, $data = array(), $echo = true) {
+    public function render($templates, $data = array(), $echo = true)
+    {
     }
 
-    public function isRenderDirectly() {
+    public function isRenderDirectly()
+    {
         return false;
     }
 }
