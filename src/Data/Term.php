21dc1e7ac6433f7605be0e7dad3f0faeabbf4d19
Puleeno Nguyen <puleeno@gmail.com>
2021-06-11 17:16:26 +0700

    Define term data for template engine

---
+++ src/Data/Term.php
@@ -0,0 +1,38 @@
+<?php
+namespace Jankx\TemplateEngine\Data;
+
+use WP_Term;
+
+class Term {
+    protected $term;
+
+    public function __construct($term) {
+        $this->term = &$term;
+    }
+
+    public function __isset($name) {
+        return isset($this->term->$name);
+    }
+
+    public function __get($name) {
+        return $this->term->$name;
+    }
+
+    public function __toString()
+    {
+        if (!is_a($this->term, WP_Term::class)) {
+            return '';
+        }
+
+        return sprintf(
+            '<a href="%1$s" title="%2$s">%2$s</a>',
+            get_term_link($this->term),
+            $this->term->name
+        );
+    }
+
+    public function link()
+    {
+        return get_term_link($this->term);
+    }
+}
